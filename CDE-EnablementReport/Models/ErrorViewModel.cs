b7fd3790216b9c5ad8d160c7b4648fa314270ce2
pvswaminath <venkataswaminath@gmail.com>
2018-03-23 14:07:59 +0530

    Add project files.

---
+++ CDE-EnablementReport/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CDE_EnablementReport.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
