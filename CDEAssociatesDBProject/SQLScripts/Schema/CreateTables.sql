c82eb22606e1a1ff63ef5a4c3758bdd86d0f4de4
P Venkata Swaminath <37394609+pvswaminath@users.noreply.github.com>
2018-03-26 19:40:25 +0530

    Updated table Schema

--- CDEAssociatesDBProject/SQLScripts/Schema/CreateTables.sql
+++ CDEAssociatesDBProject/SQLScripts/Schema/CreateTables.sql
@@ -1,21 +1,21 @@
 ﻿IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'AssociateCourseDetails')
 BEGIN
   CREATE TABLE [dbo].[AssociateCourseDetails] (  
-     [ID]										UNIQUEIDENTIFIER NOT NULL,  
-     [AssociateID]								INT  NOT NULL,  
-     [AssociateName]							VARCHAR NOT NULL,
-	 [Course]									VARCHAR NULL,
-	 [Stack]									VARCHAR NULL,
-	 [Training_Vendor]							VARCHAR NULL,	 
-	 [Recommendation]							VARCHAR NULL,
-	 [Reporting_Status]							VARCHAR NULL,	 
-	 [Grade2]									VARCHAR NULL,
-	 [Level_Description]						VARCHAR NULL,
-	 [DepartmentDescription]					VARCHAR NULL,
-	 [DepartmentGroup]							VARCHAR NULL,
-	 [Classification]							VARCHAR NULL,
-	 [HorizontalName]							VARCHAR NULL,
-	 [VerticalName]								VARCHAR NULL,	 
+     [ID]										INT IDENTITY(1, 1) NOT NULL,  
+     [AssociateID]								VARCHAR(50)  NOT NULL,  
+     [AssociateName]							VARCHAR(50) NOT NULL,
+	 [Course]									VARCHAR(50) NULL,
+	 [Stack]									VARCHAR(50) NULL,
+	 [Training_Vendor]							VARCHAR(50) NULL,	 
+	 [Recommendation]							VARCHAR(50) NULL,
+	 [Reporting_Status]							VARCHAR(50) NULL,	 
+	 [Grade2]									VARCHAR(50) NULL,
+	 [Level_Description]						VARCHAR(50) NULL,
+	 [DepartmentDescription]					VARCHAR(50) NULL,
+	 [DepartmentGroup]							VARCHAR(50) NULL,
+	 [Classification]							VARCHAR(50) NULL,
+	 [HorizontalName]							VARCHAR(50) NULL,
+	 [VerticalName]								VARCHAR(50) NULL,	 
      CONSTRAINT [PK_dbo.AssociateCourseDetails] PRIMARY KEY CLUSTERED ([ID] ASC)  
  )   
 END
